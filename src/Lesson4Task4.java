25937a81ab19d96b4cc565538573b9f7b124aba4
Ruslan Zhabbarou <privetvcem1234@gmail.com>
2022-12-30 13:13:59 +0300

    Lesson4Task1-4 fixed

--- src/Lesson4Task4.java
+++ src/Lesson4Task4.java
@@ -4,7 +4,7 @@ public class Lesson4Task4 {
         int mnozhitel2 = 1;
         int proizvedenie = 0;
         while (mnozhitel1 <= 10) {
-            while (mnozhitel2 <= 10){
+            while (mnozhitel2 <= 10) {
                 proizvedenie = mnozhitel1 * mnozhitel2;
                 System.out.println(mnozhitel1 + " * " + mnozhitel2 + " = " + proizvedenie);
                 mnozhitel2++;
