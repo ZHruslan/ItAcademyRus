93123efccdb004bce267125c95354ba5436f2fc4
Ruslan Zhabbarou <privetvcem1234@gmail.com>
2023-01-09 11:23:42 +0300

    Lesson4 fix tast03/04

--- src/Lesson4Task3.java
+++ src/Lesson4Task3.java
@@ -1,11 +1,11 @@
 public class Lesson4Task3 {
     public static void main(String[] args) {
-        int fibonachi = 1;
-        int cikl = 0;
-        while (cikl < 10) {
+        int fibonachiNumber = 1;
+        int amountNumber = 0;
+        while (amountNumber < 10) {
             System.out.println(fibonachi);
-            fibonachi = fibonachi + fibonachi;
-            cikl++;
+            fibonachiNumber = fibonachiNumber + fibonachiNumber;
+            amountNumber++;
         }
     }
 }
--- src/Lesson4Task4.java
+++ src/Lesson4Task4.java
@@ -1,16 +1,16 @@
 public class Lesson4Task4 {
     public static void main(String[] args) {
-        int mnozhitel1 = 1;
-        int mnozhitel2 = 1;
-        int proizvedenie = 0;
-        while (mnozhitel1 <= 10) {
-            while (mnozhitel2 <= 10) {
-                proizvedenie = mnozhitel1 * mnozhitel2;
-                System.out.println(mnozhitel1 + " * " + mnozhitel2 + " = " + proizvedenie);
-                mnozhitel2++;
+        int firstFactor = 1;
+        int secondFactor = 1;
+        int product = 0;
+        while (firstFactor <= 10) {
+            while (secondFactor <= 10) {
+                product = firstFactor * secondFactor;
+                System.out.println(firstFactor + " * " + secondFactor + " = " + product);
+                secondFactor++;
             }
-            mnozhitel1++;
-            mnozhitel2 = 1;
+            firstFactor++;
+            secondFactor = 1;
         }
     }
 }
