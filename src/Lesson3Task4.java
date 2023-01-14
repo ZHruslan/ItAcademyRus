396c0b7bb5fdcc217c78f8a03db6ab9c43c0ab28
Ruslan Zhabbarou <privetvcem1234@gmail.com>
2023-01-14 11:50:46 +0300

    Task4 Fix

--- src/Lesson3Task4.java
+++ src/Lesson3Task4.java
@@ -1,76 +1,38 @@
 public class Lesson3Task4 {
     public static void main(String[] args) {
-        int hour = 13;
+        int hour = 3;
         switch (hour) {
             case 1:
-                System.out.println("Good night!");
-                break;
             case 2:
-                System.out.println("Good night!");
-                break;
             case 3:
-                System.out.println("Good night!");
-                break;
             case 4:
-                System.out.println("Good night!!");
-                break;
             case 5:
                 System.out.println("Good night!");
                 break;
             case 6:
-                System.out.println("Good morning!");
-                break;
             case 7:
-                System.out.println("Good morning!");
-                break;
             case 8:
-                System.out.println("Good morning!");
-                break;
             case 9:
-                System.out.println("Good morning!");
-                break;
             case 10:
-                System.out.println("Good morning!");
-                break;
             case 11:
                 System.out.println("Good morning!");
                 break;
             case 12:
-                System.out.println("Good day!");
-                break;
             case 13:
-                System.out.println("Good day!");
-                break;
             case 14:
-                System.out.println("Good day!");
-                break;
             case 15:
-                System.out.println("Good day!");
-                break;
             case 16:
-                System.out.println("Good day!");
-                break;
             case 17:
                 System.out.println("Good day!");
                 break;
             case 18:
-                System.out.println("Good evening!");
-                break;
             case 19:
-                System.out.println("Good evening!");
-                break;
             case 20:
-                System.out.println("Good evening!");
-                break;
             case 21:
                 System.out.println("Good evening!");
                 break;
             case 22:
-                System.out.println("Good night!");
-                break;
             case 23:
-                System.out.println("Good night!");
-                break;
             case 0:
                 System.out.println("Good night!");
                 break;
